f8b645ae234eaad0e30deeb2e435a9177d554fe0
usalman <umit.salman@bil.omu.edu.tr>
2021-12-08 00:20:55 +0300

    change the todo's completed property when button clicked

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,10 +7,20 @@ function Todo({ text, todos, setTodos, todo }) {
     setTodos(todos.filter(el => el.id !== todo.id))
   }
 
+  const handleComplete = (e) => {
+    e.preventDefault()
+    setTodos(todos.map(item => {
+      if(item.id === todo.id) {
+        return { ...item, completed: !item.completed }
+      }
+      return item
+    }))
+  }
+
   return (
     <div>
       <li className="todoItem">{text}
-        <button>Complete</button>
+        <button onClick={handleComplete}>Complete</button>
         <button onClick={handleDelete}>Delete</button>
       </li>
     </div>
